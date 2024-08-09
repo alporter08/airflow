954df965653e8fa7140b7f004f77bb8b3241b724
Adrien Porter <adrien.porter@gmail.com>
2024-08-09 08:49:12 -0700

    add fred index data pull and refactor process_stocks_data

--- dags/stocks_feed_dag_v2.py
+++ dags/stocks_feed_dag_v2.py
@@ -26,10 +26,22 @@ stocks_data_dtypes = {
     "n": "float64",
 }
 
+index_data_dtypes = {}
+
+
+def process_stocks_data(path):
+    import pandas as pd
+
+    with path.open("r") as json_file:
+        data = json.load(json_file)
+    df = pd.DataFrame(data["results"]).astype(stocks_data_dtypes)
+    print(df.head())
+    return df
+
 
 @dag(
-    schedule=timedelta(days=1),
-    start_date=pendulum.datetime(2024, 5, 1, tz="US/Eastern"),
+    schedule=timedelta(days=1),  # AfterWorkdayTimetable()
+    start_date=pendulum.datetime(2024, 7, 26, tz="US/Eastern"),
     catchup=True,
     tags=["prod"],
 )
@@ -45,7 +57,7 @@ def stocks_feed_dag_v2():
     @task
     def get_stocks_data(**kwargs) -> ObjectStoragePath:
         """
-        Get stocks data from Polygon API. The data is saved as parquet.
+        Get stocks data from Polygon API. The data is saved as json.
         """
 
         API_KEY = Variable.get("POLYGON_API_KEY")
@@ -62,7 +74,33 @@ def stocks_feed_dag_v2():
         base.mkdir(exist_ok=True)
 
         formatted_date = start_date.format("YYYYMMDD")
-        path = base / "daily_stocks_raw" / f"{formatted_date}.json"
+        path = base / "daily_stocks_raw" / f"{formatted_date}_daily_stocks_raw.json"
+
+        with path.open("w") as file:
+            json.dump(data, file)
+
+        time.sleep(13)
+        return path
+
+    @task
+    def get_index_data(**kwargs) -> ObjectStoragePath:
+        """
+        Get SP500 data from FRED API. The data is saved as json.
+        """
+
+        start_date = str(kwargs["data_interval_start"].date())
+
+        API_KEY = Variable.get("FRED_API_KEY")
+
+        url = f"https://api.stlouisfed.org/fred/series/observations?series_id=SP500&api_key={API_KEY}&file_type=json"
+        r = requests.get(url, timeout=60)
+        data = r.json()
+
+        # ensure the bucket exists
+        base.mkdir(exist_ok=True)
+
+        formatted_date = start_date.format("YYYYMMDD")
+        path = base / "fred_sp500_raw" / f"{formatted_date}_fred_sp500_raw.json"
 
         with path.open("w") as file:
             json.dump(data, file)
@@ -73,15 +111,10 @@ def stocks_feed_dag_v2():
     @task
     def make_parquet(path: ObjectStoragePath, **kwargs):
 
-        import pandas as pd
-
         start_date = str(kwargs["data_interval_start"].date())
         formatted_date = start_date.format("YYYYMMDD")
 
-        with path.open("r") as json_file:
-            data = json.load(json_file)
-        df = pd.DataFrame(data["results"]).astype(stocks_data_dtypes)
-        print(df.head())
+        df = process_stocks_data(path)
         target_path = base / "daily_stocks_parquet" / f"{formatted_date}.parquet"
         target_path.parent.mkdir(exist_ok=True)
 
@@ -91,6 +124,7 @@ def stocks_feed_dag_v2():
 
     obj_path = get_stocks_data()
     make_parquet(obj_path)
+    obj_path = get_index_data()
 
 
 dag_object = stocks_feed_dag_v2()
